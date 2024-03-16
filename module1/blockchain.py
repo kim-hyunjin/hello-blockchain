1a72e3743072e1c2eb56c18ca3ff48508f0dbfe6
kim-hyunjin <hyunjin1612@gmail.com>
2024-03-16 23:08:33 +0900

    작업증명 함수

--- module1/blockchain.py
+++ module1/blockchain.py
@@ -20,5 +20,28 @@ class Blockchain:
         }
         return block
 
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            # 더 복잡한 계산식을 이용해 난이도를 높일 수 있음
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode()
+            ).hexdigest()
+            # leading zero의 개수가 많을수록 난이도가 높음
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
 
 # Part 2 - Mining our Blockchain
