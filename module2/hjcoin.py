78b45e3b0f5641fb00b8e4e9259c6d5352e5e359
kim-hyunjin <hyunjin1612@gmail.com>
2024-03-30 14:41:29 +0900

    transaction 추가 api

--- module2/hjcoin.py
+++ module2/hjcoin.py
@@ -109,6 +109,8 @@ class Blockchain:
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
+node_address = str(uuid4()).replace("-", "")
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
@@ -125,6 +127,7 @@ def mine_block():
     previous_proof = previous_block["proof"]
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver="HJ", amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {
         "message": "Congratulations, you just mined a block!",
@@ -132,6 +135,7 @@ def mine_block():
         "timestamp": block["timestamp"],
         "proof": block["proof"],
         "previous_hash": block["previous_hash"],
+        "transactions": block["transactions"],
     }
     return jsonify(response), 200
 
@@ -141,6 +145,17 @@ def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
     response = {"is_valid": is_valid}
     return jsonify(response), 200
+
+
+@app.route("/add_transaction", methods=["POST"])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ["sender", "receiver", "amount"]
+    if not all(key in json for key in transaction_keys):
+        return "Some elements of the transaction are missing", 400
+    index = blockchain.add_transaction(json["sender"], json["receiver"], json["amount"])
+    response = {"message": f"This transaction will be added to Block {index}"}
+    return jsonify(response), 201
 
 
 # Part 3 - Decentralizing our Blockchain
